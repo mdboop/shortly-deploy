5ff41424f7f190ff90f23ca0c49022619c24eada
Hack Reactor Students <accounts@hackreactor.com>
2015-09-16 22:28:14 -0700

    More mongo refactoring

--- app/mongoose.js
+++ app/mongoose.js
@@ -2,24 +2,47 @@ var mongoose = require('mongoose');
 var path = require('path');
 
 //replace with what we have from mongolabs
-mongoose.connect('mongodb://localhost:4568');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 //SCHEMA
 
 //urls
 
 //users schema
-exports.User = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
 
 //links schema
 
-exports.Link = new mongoose.Schema({
+var linkSchema = new mongoose.Schema({
   url: String,
   base_url: String,
   code: String,
   title: String,
   visits: Number
 });
+
+exports.User = mongoose.model('User', userSchema);
+
+exports.Link = mongoose.model('Link', linkSchema);
+
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.find('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+userSchema.methods.hashPassword = function(){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.find('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+};
